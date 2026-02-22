f5deac0804d187aa9cc7680b3a0dfadeb2d7f9aa
akshay-Kalekar <akalekar2003@gmail.com>
2026-02-22 09:46:00 +0530

    higher level lookup of redux

---
+++ src/SetCounter.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { set } from "./actions";
+import { useState } from "react";
+import store from "./store";
+
+export const SetCounter = () => {
+  const [counter, setCounter] = useState(0);
+  const dispatch = useDispatch();
+  const state = useSelector((s) => s.counter);
+
+  return (
+    <section className="controls">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          dispatch(set(Number(counter)));
+        }}
+      >
+        <label htmlFor="set-to">Set Count</label>
+        <input
+          id="set-to"
+          type="number"
+          value={counter}
+          onChange={(e) => setCounter(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+      <h1>{state}</h1>
+    </section>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Application from "./Application";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./index.scss";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <Application />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root"),
+);
